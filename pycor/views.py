0d6df153130f8e07f9327deb73044f187b377766
hamjimin <hua4187@naver.com>
2021-09-23 15:13:31 +0900

    data Extraction claer but update need

--- pycor/views.py
+++ pycor/views.py
@@ -1,13 +1,23 @@
 import os
+import pymysql
+
+import pytesseract
+import re
+from .models import Djangotest
 from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render
 from datetime import datetime
 from PIL import Image
-import pytesseract
-import re
-from .models import Djangotest
-from django.db import connection # 쿼리를 사용하기 위한 import
 
+# 이미지 흑백의 대한 스케일을 올리기 위한 import
+import numpy as np
+from matplotlib import pyplot as plt
+import cv
+import cv2
+
+from django.db import connection # 쿼리를 사용하기 위한 import
+# 돌아가는 알고리즘
+# 1, 일단 받아오는 이름을 기준으로 조회 있으면 텍스트문으로 -> 없어도 텍스트문으로 하지만 html에서 표시하기 즉, 비동기로 이루어져야함
 
 def home(request):
     context = {}
@@ -20,9 +30,12 @@ def ocr_upload(request):
     filename = datetime.now()
 
     imgname = ''
-    resulttext = ''
+    resulttext = [] # 약품의 이름 들고오기,
+    Medicine_code = ' '
     wordlist = []
     textlist = []
+
+    color = ('b', 'g', 'r')
     if 'fileSelect' in request.FILES:
         fileSelect = request.FILES.get('fileSelect', '')
 
@@ -33,25 +46,37 @@ def ocr_upload(request):
             name_ext = os.path.splitext(name_old)[1] # png, jpg, jpeg 등 이미지 확장작만 받을 수 있게 해줘야함
 
             fs = FileSystemStorage(location='static/source') # 파일을 받아와 static/source 밑에 저장하는 과정
+
             imgname = fs.save(f"src_{name_old}", fileSelect) # 파일 이름을 원래의 파일이름이 아닌 저장시 알 수 있게 src_를 붙힌다.
 
             imgfile = Image.open(f"./static/source/{imgname}")
 
-            resulttext = re.sub(r"[a-zA-Z0-9!@#%※$/(){}_]", ' ',pytesseract.image_to_string(imgfile, lang='kor')).replace("\n", '')
+            resulttext = re.sub(r"[a-zA-Z0-9!@#%※$/{}_.]",' ',pytesseract.image_to_string(imgfile, lang='kor')).replace(" ", '').replace("\n", " ").replace("내복",'').replace("점","정").replace("전","정")
 
-            for i in resulttext:
-                if i.endswith('정'):
-                    textlist.append(i)
+            # data = resulttext.replace("\n", " ").replace("내복",'').replace("점","정").replace("전","정")
 
+            resulttext = resulttext.split()
+
+            for word in resulttext:
+                   if word.endswith("정"):
+                       wordlist.append(word)
+                   elif word.endswith("주"):
+                       wordlist.append(word)
+
+            Medicine_code = re.sub(r"[^0-9]",' ',pytesseract.image_to_string(imgfile, lang='kor')).replace("\n"," ")
+
+            # image = cv2.imread(fileSelect, cv2.IMREAD_GRAYSCALE) # 이미지 흑백으로 만들기
+            #
+            # image_enhanced = cv2.equalizeHist(image)
+
+            # plt.imshow(image_enhanced, cmap="gray"),plt.axis("off")
+    # OCR의 기준으로 정, 점, 전으로 끝나는 단어들 불러와야함
+    context['Medicine_code'] = Medicine_code.split()
     context['imgname'] = imgname
     context['dbsavefilename'] = filename
-    context['resulttext'] = resulttext.replace("정", '정,').replace(" ",'')
-    context['textlist'] = textlist
-    for word in resulttext:
-        if word.endswith('정'):
-            wordlist.append(word)
+    context['resulttext'] = resulttext
+    context['wordlist'] =  wordlist
 
-    context['wordlist'] = wordlist
     return render(request, 'ocr_upload.html', context)
 
 
@@ -70,7 +95,7 @@ def FindPass(request):
     context = {}
     context['menutitle'] = 'OCR_List'
 
-    reSource = Djangotest.object.fliter()
+    reSource = Djangotest.objectn.fliter()
     context['reSource'] = reSource
 
     return render(request, 'ocr_list.html', context)
